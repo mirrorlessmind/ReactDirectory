a7a9f9d6f5a793f894e4ba11bca56679ee2a7c17
Cigi <cigi@mirrorlessmind.com>
2021-06-23 20:21:56 -0500

    Initial App file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header";
+import Table from "./components/table"; 
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Table />
+    </div>
+  );
+}
+
+export default App;
